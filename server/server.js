4ca2560ebc6c9c015bdc2849b71c4258b25167da
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-04-26 16:01:57 -0500

    server.js now mirrors previous repository.

--- server/server.js
+++ server/server.js
@@ -2,9 +2,20 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+//creates the apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+//lets the server know that there will be middleware connecting it to the app
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +25,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+
+//replaces app.use(routes)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
