2cacfae438c1f47632c0c5eac1ed799c3063653a
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-04-26 19:22:37 -0500

    client signUpForm.js now reading  useMutation hook

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,7 +1,7 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import { useMutation } from '@apollo/react-hooks';
+import { useMutation } from '@apollo/client';
 import { LOGIN_USER  } from '../utils/mutations';
 import Auth from '../utils/auth';
 
@@ -31,11 +31,7 @@ const LoginForm = () => {
     }
     // check if form has everything (as per react-bootstrap docs)
     //TODO: May need to delete 34-53
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+    
 
     try {
       const response = await loginUser(userFormData);
